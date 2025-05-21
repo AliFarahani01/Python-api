bdd4656aa62a9f9a436d159b10aad0678f8bee74
AliFarahani01 <alifarahani31301@gmail.com>
2025-05-21 13:16:30 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ import io
 import traceback
 import contextlib
 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Render!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 app = Flask(__name__)
 
 @app.route('/')
